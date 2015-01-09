fb39189d71401e0dca5a5b11477817e8f2d257c5
Henrik Wingerei <henrik.wingerei@bekk.no>
2015-01-09 11:50:33 +0100

    Sends only top trhee hashtags to client. Uses hashtags entitites instead of splitting on space

--- src/main/java/no/bekk/bootcamp/SparkApplication.java
+++ src/main/java/no/bekk/bootcamp/SparkApplication.java
@@ -1,18 +1,11 @@
 package no.bekk.bootcamp;
 
 import org.apache.spark.SparkConf;
-import org.apache.spark.api.java.JavaPairRDD;
-import org.apache.spark.api.java.JavaRDD;
-import org.apache.spark.api.java.function.Function;
 import org.apache.spark.streaming.Durations;
-import org.apache.spark.streaming.api.java.JavaDStream;
-import org.apache.spark.streaming.api.java.JavaPairDStream;
-import org.apache.spark.streaming.api.java.JavaReceiverInputDStream;
 import org.apache.spark.streaming.api.java.JavaStreamingContext;
-import org.apache.spark.streaming.dstream.DStream;
 import org.apache.spark.streaming.twitter.TwitterUtils;
 import scala.Tuple2;
-import twitter4j.Status;
+import twitter4j.HashtagEntity;
 
 import java.io.IOException;
 import java.util.Properties;
@@ -27,37 +20,21 @@ public class SparkApplication {
         System.getProperties().putAll(twitterProperties);
 
         SparkConf conf = new SparkConf().setMaster("local[2]").setAppName("twitterApp");
-        JavaStreamingContext streamingContext = new JavaStreamingContext(conf, Durations.seconds(15));
+        JavaStreamingContext streamingContext = new JavaStreamingContext(conf, Durations.milliseconds(100));
 
-        JavaReceiverInputDStream<Status> stream = TwitterUtils.createStream(streamingContext);
+        TwitterUtils.createStream(streamingContext)
+                .window(Durations.seconds(60), Durations.seconds(5))
+                .flatMap(status -> asList(status.getHashtagEntities()))
+                .countByValue()
+                .foreach(rdd -> {
+                    rdd.map(Tuple2::swap)
+                            .sortBy(tuple -> tuple._1, false, 1)
+                            .take(3)
+                            .forEach(tuple -> CustomWebSocketServlet.broadcastMessage(tuple._2.getText() + "," + tuple._1));
+                    return null;
+                });
 
-        JavaDStream<String> hashTags = stream.flatMap(status -> asList(status.getText().split(" ")))
-                .filter(word -> word.startsWith("#"));
-        JavaPairDStream<String, Long> counted = hashTags.countByValue();
-
-        counted.foreach(rdd -> {
-            JavaRDD<Tuple2<Long, String>> freqAndWord = rdd.map(tuple -> tuple.swap());
-            System.out.println("---------------------------");
-            JavaRDD<Tuple2<Long, String>> sorted = freqAndWord.sortBy(tuple -> tuple._1(), false, 1);
-            sorted.foreach(tuple -> {
-                System.out.println(tuple._2() + ",  " + tuple._1());
-
-                CustomWebSocketServlet.broadcastMessage(tuple.toString());
-            });
-            return null;
-        });
-        
         streamingContext.start();
         streamingContext.awaitTermination();
-    }
-    
-    class HashTag {
-        public final String word;
-        public final int count;
-
-        public HashTag(String word, int count) {
-            this.word = word;
-            this.count = count;
-        }
     }
 }
